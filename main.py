d4e27364bcb156c79a4c9972cba37e05f3b9c2c8
PierrePARAJUA <pierre.parajua34@gmail.com>
2022-12-09 14:05:33 +0100

    V1

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ from utils import modified_indentation
 
 SOURCE_FOLDER = "document_source"
 RESULT_FOLDER = "result"
-
+EXCEL_FILE = "EmployersRequirementsExcelMaster.xlsx"
 
 # Définie les chemins
-PATH_RESULT = Path(__file__).resolve().parent / RESULT_FOLDER
-PATH_DOCUMENT_SOURCE = Path(__file__).resolve().parent / SOURCE_FOLDER
+PATH_RESULT = Path(__file__).resolve().parent.parent / RESULT_FOLDER
+PATH_DOCUMENT_SOURCE = Path(__file__).resolve().parent.parent / SOURCE_FOLDER
+PATH_EXCEL_FILE = Path(__file__).resolve().parent.parent / EXCEL_FILE
+
 
 def get_df_from_excel():
     # Crée un dataframe à partir du fichier Excel
-    df = pd.read_excel('EmployersRequirementsExcelMaster.xlsx', sheet_name="database", header=1)
+    df = pd.read_excel(PATH_EXCEL_FILE, sheet_name="database", header=1)
 
     # recré les indentations pour les paragraphes
-    df['Requirement']= df['Requirement'].map(modified_indentation)
-    df['Exigence']= df['Exigence'].map(modified_indentation)
+    df['Requirement'] = df['Requirement'].map(modified_indentation)
+    df['Exigence'] = df['Exigence'].map(modified_indentation)
 
-
-    # Crée des dictionnaires avec en clé la "Doc Reference" référence"" et en valeur "Requirement", "Exigence" et "Level"
+    # Crée des dictionnaires avec en clé la "Doc Reference" référence"" et en valeur "Requirement", "Exigence" et
+    # "Level"
     zip_requirement = zip(df['Doc Reference'], df['Requirement'])
     zip_exigence = zip(df['Doc Reference'], df['Exigence'])
     zip_level = zip(df['Doc Reference'], df['Level'])
@@ -30,7 +32,6 @@ def get_df_from_excel():
     return requirement, exigence, level
 
 
-
 # Liste tous les fichiers présent dans le dossier en paramètre
 def get_files_name(folder_path):
     files_name = []
@@ -38,12 +39,14 @@ def get_files_name(folder_path):
         files_name.append(f.name)
     return files_name
 
+
 # Crée une instance de Document du word template
 def create_word_instances(files):
     word_instances = []
     for w in files:
         word_instances.append(Document(PATH_DOCUMENT_SOURCE / w))
     return word_instances
+
 
 #  Liste contenant tout les objets cells du document word
 def get_all_cells(document: Document):
@@ -53,6 +56,7 @@ def get_all_cells(document: Document):
             for cell in row.cells:
                 cells.append(cell)
     return cells
+
 
 # remplit les oblets cells avec le text extrait du excel
 def create_modified_template(cells, requirement, exigence, level):
@@ -66,17 +70,17 @@ def create_modified_template(cells, requirement, exigence, level):
         if cell.text in level.keys():
             ind = cells.index(cell) + 1
             cells[ind].text = requirement[cell.text]
-    
 
 
 
-if __name__ == "__main__":
-    requirement, exigence, level = get_df_from_excel()
-    files_name = get_files_name(PATH_DOCUMENT_SOURCE)
-    words_instances = create_word_instances(files_name)   
-    for instance, file_name in zip(words_instances, files_name):
-        cells = get_all_cells(instance)
-        create_modified_template(cells, requirement, exigence, level)
-        instance.save(PATH_RESULT / file_name)
-
-
+print("Lancement du programme")
+print("Chargement du fichier excel source ..")
+requirement, exigence, level = get_df_from_excel()
+print("Chargement terminé")
+files_name = get_files_name(PATH_DOCUMENT_SOURCE)
+print(f"Listes des fichiers word à modifier : {files_name}")
+words_instances = create_word_instances(files_name)
+for instance, file_name in zip(words_instances, files_name):
+    cells = get_all_cells(instance)
+    create_modified_template(cells, requirement, exigence, level)
+    instance.save(PATH_RESULT / file_name)
